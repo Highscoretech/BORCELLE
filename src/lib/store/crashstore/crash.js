7595cb295bdb86adb587b59de0ee7079406f32ee
Highscore <highscoretechng@gmail.com>
2023-08-02 14:24:43 +0100

    okayu

---
+++ src/lib/store/crashstore/crash.js
@@ -0,0 +1,10 @@
+// import { writable } from "svelte/store"
+// import axios from "axios"
+// let animation
+
+// axios.get('https://highscorecrash-animation.herokuapp.com/crashanimation')
+// .then(res =>{
+//     animation = res.data
+// })
+
+// export let imag = writable(animation)
--- src/routes/crash/crash.svelte
+++ src/routes/crash/crash.svelte
@@ -1,30 +1,60 @@
 <script>
-    	import { Stage, Layer, Text, Rect, Circle, Line } from "svelte-konva";
-        import {onMount} from "svelte";
-        let count = 200
-        let width = 518
-        let height = 310
+    import { initializeApp, getApp, getApps } from "firebase/app";
+    import { getFirestore, collection, onSnapshot } from "firebase/firestore";
+    import { firebaseConfig } from "$lib/firebaseConfig";
+    import { browser } from '$app/environment'
+    let firebaseApp = browser && (getApps().length === 0 ? initializeApp(firebaseConfig) :  getApp() ) ;
+    let db =  browser && getFirestore()
+    let colRef = browser && collection(db, "borc")
+    import { Stage, Layer, Text, Rect, Circle, Line, Image } from "svelte-konva";
+    import {onMount} from "svelte";
+    // import { animation} from "$lib/store/crashstore/crash";
+    // let images = $animation
+    let width = 518
+    let height = 310
+    let crashDB = []
 
+     browser && onSnapshot(colRef, (querySnapshot) => {
+        var fbData = [];
+        querySnapshot.forEach((doc) => {
+            let crashData = { ...doc.data(), id: doc.id }
+            fbData = [crashData , ...fbData]
+        });
+        crashDB = fbData[0]
+    });
 
-    onMount(()=>{
-     let countDown = setInterval(()=>{
-        if(count > 0){
-            count -= 3
-        }else{
-            clearInterval(countDown)
-        }
-        },100)
-    })
+    let image = null
+    let lastanim;
+    let firstanim
+    let thirdanim
+    let secanim;
+    // onMount(()=>{
+    //     img = document.createElement("img");
+    // })
+
+    // $: {
+    //     if(crashDB.isRunning){   
+    //         if(crashDB.animCrash >= images.length ){
+    //             img.src = images[images.length - 1]
+    //             img.onload = () => {
+    //                 image = img;
+    //             }
+    //         }else{
+    //             img.src = images[crashDB.animCrash]
+    //             img.onload = () => {
+    //                 image = img;
+    //             }
+    //         }
+    //     }
+    // }
 
     let stepTwo = (1.2.toFixed(2))
     let stepThree = (1.30).toFixed(2)
     let stepFour = (1.50).toFixed(2)
     let stepFive = (1.7).toFixed(2)
     let stepSix = (1.8).toFixed(2)
-
-
-
 </script>
+
 
 <div class="game-box sbuagsugwq">
     <div class="game-box-container">
@@ -34,47 +64,99 @@
         <div class="chart-crash">
             <Stage config={{ width: width, height:height }}>
                 <Layer>
-                    <Rect config={{
-                        x: width / 2 - 100,
-                        y: 160,
-                        width: 200,
-                        height: 40,
-                        fill: 'grey',
-                        cornerRadius: 3,
-                      }}
-                    />
-                    <Rect config={{
-                        x: 518 / 2 - 100,
-                        y: 160,
-                        width: count,
-                        height: 40,
-                        fill: '#43b309',
-                        shadowBlur: 10,
-                        cornerRadius: 3,
-                      }}
-                    />
-                    <Text config={{
-                        text: '1.00x',
-                        fontSize: 50,
-                        fill: '#ffff',
-                        shadowBlur: 10,
-                        x: 200,
-                        y: 80,
-                        fontStyle: '700',
-                        fontFamily: 'Calibri',
-                    }}/>
-                  
-                  <Text config={{
-                    text: 'Starting in',
-                    fontSize: 23,
-                    fill: '#ffff',
-                    shadowBlur: 1,
-                    x: 210,
-                    y: 170,
-                    fontStyle: '400',
-                    fontFamily: 'Calibri',
-                }}/>
+                    {#if (!crashDB.isLoading && !crashDB.isRunning && !crashDB.isCrash ) }
+                        <Text config={{
+                            text: 'Reconnecting...' ,
+                            fontSize: 25,
+                            fill: '#ffff',
+                            shadowBlur: 10,
+                            x: 160,
+                            y: 130,
+                            fontStyle: '500',
+                            fontFamily: 'Calibri',
+                        }}/>
+                    {/if}
+                    <!-- ========================== loading crash ====================== -->
+                    {#if (crashDB.isLoading)}
+                        <Rect config={{
+                            x: width / 2 - 100,
+                            y: 160,
+                            width: 200,
+                            height: 40,
+                            fill: 'grey',
+                            cornerRadius: 3,
+                        }}
+                        />
 
+                        <Text config={{
+                            text: '1.00x',
+                            fontSize: 50,
+                            fill: '#ffff',
+                            shadowBlur: 10,
+                            x: 200,
+                            y: 80,
+                            fontStyle: '700',
+                            fontFamily: 'Calibri',
+                        }}/>
+
+                        <Rect config={{
+                            x: 518 / 2 - 100,
+                            y: 160,
+                            width: crashDB.loadingCount,
+                            height: 40,
+                            fill: '#43b309',
+                            shadowBlur: 10,
+                            cornerRadius: 3,
+                        }}
+                        />
+                        
+                        <Text config={{
+                            text: 'Starting in',
+                            fontSize: 23,
+                            fill: '#ffff',
+                            shadowBlur: 1,
+                            x: 210,
+                            y: 170,
+                            fontStyle: '400',
+                            fontFamily: 'Calibri',
+                        }}/>
+                    {/if}
+                    
+                    <!-- =========================== crash is running ======================== -->
+                        {#if (crashDB.isRunning)}
+                            <!-- <Image
+                            config={{ image : image ? image : "",
+                                x: 20,
+                                y: 0, }}
+                            /> -->
+                        
+                            <Text config={{
+                                text: crashDB.runningCount + 'x',
+                                fontSize: 50,
+                                fill: '#ffff',
+                                shadowBlur: 10,
+                                x: 200,
+                                y: 80,
+                                fontStyle: '700',
+                                fontFamily: 'Calibri',
+                            }}/>
+                        {/if}
+
+                <!-- =================================== isCrash ====================================== -->
+                        {#if (crashDB.isCrash)}
+                            <Text config={{
+                                text: 'Crashed at ' + crashDB.crashPoint + "x" ,
+                                fontSize: 30,
+                                fill: 'rgb(237, 99, 0)',
+                                shadowBlur: 10,
+                                x: 150,
+                                y: 130,
+                                fontStyle: '700',
+                                fontFamily: 'Calibri',
+                            }}/>
+                        {/if}
+                
+                    <!-- ============= Crash Lines ============ -->
     
                     <Line config={{
                       x: 20,
@@ -146,7 +228,7 @@
             }}
             />
             <Text config={{
-                text: stepThree + "x",
+                text: crashDB.forthY ? crashDB.forthY + "x" : stepThree + "x",
                 fontSize: 12,
                 fill: '#ffff',
                 shadowBlur: 10,
@@ -168,7 +250,7 @@
 }}
 />
 <Text config={{
-    text: stepFour + "x",
+    text: crashDB.forthY ? crashDB.forthY + "x" : stepFour + "x",
     fontSize: 12,
     fill: '#ffff',
     shadowBlur: 10,
@@ -190,7 +272,7 @@
 }}
 />
 <Text config={{
-    text: stepFive + "x",
+    text: crashDB.fifthY ? crashDB.fifthY + "x" : stepFive + "x",
     fontSize: 12,
     fill: '#ffff',
     shadowBlur: 10,
@@ -211,7 +293,7 @@
 }}
 />
         <Text config={{
-            text: stepSix + "x",
+            text: crashDB.sixthY ? crashDB.sixthY + "x" : stepSix + "x",
             fontSize: 12,
             fill: '#ffff',
             shadowBlur: 10,
@@ -222,19 +304,20 @@
         }}/>
 
 
-        <Text config={{
-            text: '2s',
-            fontSize: 12,
-            fill: '#ffff',
-            shadowBlur: 10,
-            x: 100,
-            y: 299,
-            fontStyle: '900',
-            fontFamily: 'Calibri',
-        }}/>
+<!-- ============================== Seconds at the buttom ========================================== -->
+<Text config={{
+    text: crashDB.twoSec ? crashDB.twoSec + "s" : `2s`,
+    fontSize: 12,
+    fill: '#ffff',
+    shadowBlur: 10,
+    x: 100,
+    y: 299,
+    fontStyle: '900',
+    fontFamily: 'Calibri',
+}}/>
 
 <Text config={{
-    text: '4s',
+    text: crashDB.fourSec ? crashDB.fourSec + "s" : `4s`,
     fontSize: 12,
     fill: '#ffff',
     shadowBlur: 10,
@@ -245,7 +328,7 @@
 }}/>
 
 <Text config={{
-    text: '6s',
+    text: crashDB.sixSec ? crashDB.sixSec + "s" : `6s`,
     fontSize: 12,
     fill: '#ffff',
     shadowBlur: 10,
@@ -256,7 +339,7 @@
 }}/>
 
 <Text config={{
-    text: '8s',
+    text: crashDB.eightSec ? crashDB.eightSec + "s" : `8s`,
     fontSize: 12,
     fill: '#ffff',
     shadowBlur: 10,
@@ -267,7 +350,7 @@
 }}/>
 
 <Text config={{
-    text: 'Total 0s',
+    text: crashDB.totalSeconds ? "Total " + crashDB.totalSeconds + "s" : `Total 0s` ,
     fontSize: 12,
     fill: '#ffff',
     shadowBlur: 10,
@@ -277,13 +360,15 @@
     fontFamily: 'Calibri',
 }}/>
 
-<Circle config={{
+<!-- <Circle config={{
     x: 40,
     y: 288,
     radius: 8,
     fill: '#fff'
   }}
-/>
+/> -->
+
+ <!-- =========================================== -->
 
 
                 </Layer>
